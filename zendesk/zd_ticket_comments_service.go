e654291e15a22c17184ead2b6c7517740b152e43
XiaokaiPhil <50335005+XiaokaiPhil@users.noreply.github.com>
2019-08-27 11:23:42 -0700

    [REVIEW]fix ticket comments service (#11)
    * fix ticket comments service
    * add GetAllTicketComments

--- zendesk/zd_ticket_comments_service.go
+++ zendesk/zd_ticket_comments_service.go
@@ -1,7 +1,10 @@
 package zendesk
 
 import (
+	"bytes"
 	"fmt"
+	"log"
+	"strconv"
 	"time"
 )
 
@@ -64,43 +67,81 @@ type FromObject struct {
 	Title string `json:"title"`
 }
 
-/*  For Reference. Please do not delete.
-// type MetaObject struct {
-
-// }
-
-// metadata struct in https://developer.zendesk.com/rest_api/docs/support/ticket_comments#comment-flags
-
-metadata: {
-  system: { ... },
-  flags: [2,5],
- "flags_options": {
-   "2": {
-     "trusted": false
-   },
-   "5": {
-     "message": {
-       "file": "printer_manual.pdf",
-       "account_limit": "20"
-     },
-     "trusted": false
-   }
- },
- "trusted": false,
- "suspension_type_id": null
-}
-
-*/
-
-/*
-//GET /api/v2/tickets/{ticket_id}/comments.json
-
-curl https://{subdomain}.zendesk.com/api/v2/tickets/{ticket_id}/comments.json \
--H "Content-Type: application/json" -v -u {email_address}:{password}
-*/
-
-func (c *client) ShowTicketComments(id int64) ([]TicketComment, error) {
+func (c *client) ListTicketComments(id int64) ([]TicketComment, error) {
 	out := new(APIPayload)
 	err := c.get(fmt.Sprintf("/api/v2/tickets/%d/comments.json", id), out)
-	return out.TicketComments, err
+	return out.Comments, err
+}
+
+func (c *client) GetAllTicketComments() (map[int][]TicketComment, error) {
+	ticketCommentsMap, err := c.getTicketCommentsOneByOne(nil)
+	if err != nil {
+		return nil, err
+	}
+	return ticketCommentsMap, nil
+}
+
+// getTicketCommentOneByOne return a map with ticket id as the key and
+// an array of ticket comments as its value
+func (c *client) getTicketCommentsOneByOne(in interface{}) (map[int][]TicketComment, error) {
+	endpointPrefix := "/api/v2/tickets/"
+	endpointPostfix := "/comments.json"
+
+	// we need to manually set ticketIDsForComments
+	// this step will be improved once the incremental export is done
+	var ticketIDsForComments []int = []int{}
+
+	result := make(map[int][]TicketComment)
+	payload, err := marshall(in)
+	if err != nil {
+		return nil, err
+	}
+
+	headers := map[string]string{}
+	if in != nil {
+		headers["Content-Type"] = "application/json"
+	}
+	record := new(APIPayload)
+
+	numTickets := len(ticketIDsForComments)
+	if numTickets == 0 {
+		return result, nil
+	}
+	endpoint := fmt.Sprintf("%s%v%s", endpointPrefix, ticketIDsForComments[0], endpointPostfix)
+	res, err := c.request("GET", endpoint, headers, bytes.NewReader(payload))
+	defer res.Body.Close()
+
+	var totalWaitTime int64
+	for ticketInd := 1; ticketInd < numTickets; ticketInd++ {
+		log.Printf("[ZENDESK] currently extracting: %s\n", endpoint)
+
+		// handle page not found
+		if res.StatusCode == 404 {
+			log.Printf("[ZENDESK] 404 not found: %s\n", endpoint)
+			// handle too many requests (rate limit)
+		} else if res.StatusCode == 429 {
+			after, err := strconv.ParseInt(res.Header.Get("Retry-After"), 10, 64)
+			log.Printf("[ZENDESK] too many requests. Wait for %v seconds\n", after)
+			totalWaitTime += after
+			if err != nil {
+				return nil, err
+			}
+			time.Sleep(time.Duration(after) * time.Second)
+			continue
+		} else {
+			err = unmarshall(res, record)
+			if err != nil {
+				return nil, err
+			}
+			result[ticketIDsForComments[ticketInd-1]] = record.Comments
+		}
+
+		record = new(APIPayload)
+		endpoint = fmt.Sprintf("%s%v%s", endpointPrefix, ticketIDsForComments[ticketInd], endpointPostfix)
+		res, _ = c.request("GET", endpoint, headers, bytes.NewReader(payload))
+	}
+
+	log.Printf("[ZENDESK] number of records pulled: %v\n", len(result))
+	log.Printf("[ZENDESK] total waiting time due to rate limit: %v\n", totalWaitTime)
+	return result, nil
 }
